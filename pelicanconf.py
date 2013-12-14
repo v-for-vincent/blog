3f99eecd409fadb4a4874acdea90a1df6abf54e8
Vincent Nys <vincentnys@gmail.com>
2013-12-14 16:15:24 +0100

    Preparing blog for new posting initiative.

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 
 AUTHOR = u'Vincent Nys'
 SITENAME = u'V for Vincent'
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Brussels'
 DEFAULT_LANG = u'en'
 #FEED_DOMAIN = SITEURL
 FEED_MAX_ITEMS = 10
@@ -15,8 +15,7 @@ SITESUBTITLE=u'Thoughts on programming, music, language and life in general.'
 
 # Social widget
 SOCIAL = (('LinkedIn', 'http://be.linkedin.com/pub/vincent-nys/60/50a/582/'),
-          ('Twitter', 'http://www.twitter.com/vincent_nys'),
-          ('Deviantart', 'http://pseudonym-v.deviantart.com'))
+          ('Twitter', 'http://www.twitter.com/vincent_nys'))
 
 DEFAULT_PAGINATION = 10
 STATIC_PATHS = ['images', 'attachments']
